56c1ed44a352aaf1a6a111c6413cee668359792b
Daniel Czopek <Daniel.Czopek@microsoft.wsei.edu.pl>
2022-11-03 12:00:14 +0100

    kompozyt i most skopiowany z treści zadania

---
+++ Lab_05_Kompozyt/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface Kompozyt
+{
+    //
+    void DodajElement(Kompozyt element);
+    void UsunElement(Kompozyt element);
+}
+
+public class Lisc : Kompozyt
+{
+
+    public string nazwa { get; set; }
+
+    public void Renderuj()
+    {
+        // renderowanie
+    }
+
+
+    // konstruktor
+
+    // 2 brakujące metody których wymaga interfejs
+
+}
+
+
+public class Wezel : Kompozyt
+{
+
+    private List<Kompozyt> Lista = new List<Kompozyt>();
+
+    public string nazwa { get; set; }
+
+    public void Renderuj()
+    {
+        //rozpoczęcie renderowania
+
+        //foreach item.Renderuj(); 
+
+        //zakończenie renderowania
+    }
+
+    // 2 brakujące metody 
+
+}
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        //
+        //  definicje struktury
+        //
+
+        korzen.Renderuj();
+
+    }
+}
--- Lab_05_Most/Program.cs
+++ Lab_05_Most/Program.cs
@@ -1,2 +1,129 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+public interface ITelewizor
+{
+
+    int Kanal { get; set; }
+    //
+    //
+    void ZmienKanal(int kanal);
+
+}
+
+
+
+public class TvLg : ITelewizor
+{
+
+    public TvLg()
+    {
+        this.Kanal = 1;
+    }
+
+    public int Kanal { get; set; }
+
+    public void Wlacz()
+    {
+        Console.WriteLine("Telewizor LG włączony.");
+    }
+
+    public void Wylacz()
+    {
+        //
+        //
+    }
+
+    public void ZmienKanal(int kanal)
+    {
+        //
+        //
+    }
+
+}
+
+
+
+public class TvXiaomi : ITelewizor
+{
+
+    //
+    //
+
+}
+
+
+
+public abstract class PilotAbstrakcyjny
+{
+
+    private ITelewizor tv;
+
+    public PilotAbstrakcyjny(ITelewizor tv)
+    {
+        //
+        //
+    }
+
+    //
+    //
+
+    public void ZmienKanal(int kanal)
+    {
+        //
+        //
+    }
+
+}
+
+
+
+public class PilotHarmony : PilotAbstrakcyjny
+{
+
+    public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot Harmony włącza telewizor...");
+        Wlacz();
+    }
+
+    //
+    //
+
+}
+
+public class PilotPhilips : PilotAbstrakcyjny
+{
+
+    //
+    //
+
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot Philips wyłącza telewizor...");
+        Wylacz();
+    }
+
+}
+
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        ITelewizor tv = new TvLg();
+        //
+        //
+
+        pilotHarmony.DoWlacz();
+        Console.WriteLine("Kanał: " + tv.Kanal);
+        pilotPhilips.DoZmienKanal(100);
+        Console.WriteLine("Kanał: " + tv.Kanal);
+        pilotHarmony.DoWylacz();
+
+    }
+}
