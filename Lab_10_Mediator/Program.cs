539e37f36646f0755cfb0416096d0f0a4633588c
Daniel Czopek <Daniel.Czopek@microsoft.wsei.edu.pl>
2022-12-08 12:52:56 +0100

    Mediator done

--- Lab_10_Mediator/Program.cs
+++ Lab_10_Mediator/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 
 
-//
-//
-//
+public interface IMediator
+{
+    public void DodajUzytkownika(IUzytkownik uzytkownik);
+    public void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca);
+}
 
 public class Mediator : IMediator
 {
@@ -17,21 +19,24 @@ public class Mediator : IMediator
 
     public void DodajUzytkownika(IUzytkownik uzytkownik)
     {
-        //
+        uzytkownicy.Add(uzytkownik);
         //
     }
 
     public void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca)
     {
-        //
-        //
-        //
+        foreach (IUzytkownik uzytkownik in uzytkownicy)
+        {
+            if (uzytkownik != nadawca)
+                uzytkownik.OdbierzWiadomosc(wiadomosc);
+        }
     }
 }
-
-//
-//
-//
+public interface IUzytkownik
+{
+    public void WyslijWiadomosc(string wiadomosc);
+    public void OdbierzWiadomosc(string wiadomosc);
+}
 
 public class Dev : IUzytkownik
 {
@@ -55,27 +60,49 @@ public class Dev : IUzytkownik
     }
 }
 
-//
-//
-//
+public class Klient : IUzytkownik
+{
+    string login;
+    IMediator Mediator;
+
+    public Klient(IMediator Mediator, string login)
+    {
+        this.login = login;
+        this.Mediator = Mediator;
+    }
+
+    public void WyslijWiadomosc(string wiadomosc)
+    {
+        Mediator.WyslijWiadomosc(wiadomosc, this);
+    }
+
+    public void OdbierzWiadomosc(string wiadomosc)
+    {
+        Console.WriteLine("Użytkownik " + login + " otrzymał wiadomość: " + wiadomosc);
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
 
-        //
+        Mediator mediator = new Mediator();
 
         IUzytkownik ania = new Klient(mediator, "Ania");
-        //
-        //
+        IUzytkownik nakamoto = new Dev(mediator, "Nakamoto");
+        IUzytkownik geohot = new Dev(mediator, "Geohot");
 
-        //
+        mediator.DodajUzytkownika(ania);
         mediator.DodajUzytkownika(nakamoto);
-        //
+        mediator.DodajUzytkownika(geohot);
 
-        //
-        geohot.WyslijWiadomosc("Czekam, aż Nakamoto zaparzy kawę...");
+
+        ania.WyslijWiadomosc("Proszę natychmiast wprowadzić poprawki na produkcję.");
+        geohot.WyslijWiadomosc("Czekam, aż Nakamoto zaparzy poranną kawę...");
 
     }
 }
+
+
+
